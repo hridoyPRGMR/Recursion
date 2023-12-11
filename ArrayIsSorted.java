20d16be4d936cf52d9d2f49a1f95715536190777
Hridoy <118050603+hridoyPRGMR@users.noreply.github.com>
2023-12-11 19:01:55 +0600

    Create ArrayIsSorted.java

---
+++ ArrayIsSorted.java
@@ -0,0 +1,30 @@
+public class sortedOrnot{
+
+	/*
+	public static boolean sort(int []nums,int idx,int size){
+		if(idx==size)return true;
+		if(nums[idx]<=nums[idx+1]){
+			return sort(nums,idx+1,size);
+		}
+		else{
+			return false;
+		}
+	}
+	*/
+
+	public static boolean sort(int []nums,int n){
+		if(n==0 || n==1)return true;
+
+		if(nums[n-1]<nums[n-2])return false;
+
+		return sort(nums,n-1);
+	}
+
+	public static void main(String []args){
+		int []nums={5,2};
+		int n=nums.length;
+
+		//System.out.println(sort(nums,0,n-1));
+		System.out.println(sort(nums,n));
+	}
+}
